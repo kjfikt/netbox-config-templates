5d2518feb3db634a87c45f29558e0d6f433c58c8
Tor Inge Redalen <toringe@redalen.no>
2023-07-17 15:00:37 +0200

    Create device_down.py

---
+++ scripts/device_down.py
@@ -0,0 +1,28 @@
+from django.utils.text import slugify
+
+from dcim.choices import *
+from dcim.models import Cable, Device, DeviceRole, DeviceType, Platform, Rack, RackRole, Site
+from dcim.models.device_components import FrontPort, Interface, RearPort
+
+from ipam.choices import *
+from ipam.models import Aggregate, Prefix, IPAddress, Role, VLAN
+from dcim.models import Device, DeviceRole, DeviceType, Manufacturer, Site
+
+from extras.scripts import *
+import datetime
+
+
+class DevicePing(Script):
+    #optional variables in UI here!
+    device_sn = StringVar(
+        description="Serial number for device to update"
+    )
+
+    class Meta:
+        name = "Device up"
+        description = "Device is rigged up"
+
+    def run(self, data, commit):
+        device = Device.objects.get(serial=data['device_sn'])
+        device.status = 'active'
+        device.save()
